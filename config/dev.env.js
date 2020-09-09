576b2fa0d18d4eb3887d2ec7cc2a3af3122fd253
忧郁xiao流氓 <42616678+rifengmen@users.noreply.github.com>
2020-09-09 09:00:28 +0800

    增加测试路径和模拟数据

--- config/dev.env.js
+++ config/dev.env.js
@@ -7,5 +7,7 @@ module.exports = merge(prodEnv, {
   // API_ROOT: '"http://192.168.29.118:8083/"', // 开发环境服务器地址
   // API_ROOT: '"http://192.168.1.22:8083/"', // 开发环境服务器地址
   API_ROOT: '"https://member.spzlk.cn/phptest/"', // 开发环境服务器地址
+  // API_ROOT: '"https://www.spzlk.cn/phptest/"', // 开发环境服务器地址
   VUE_APP_IMG_BASEURL: '"http://member.spzlk.cn:8000/wemember/"' // 开发环境图片基础路径
+  // VUE_APP_IMG_BASEURL: '"http://www.spzlk.cn:8000/wemember/"' // 开发环境图片基础路径
 })
